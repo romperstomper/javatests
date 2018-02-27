aa0063e732ae64e92470744c72be5265846db46b
gary boland <garyboland@gmail.com>
2018-02-27 22:27:05 +0000

    early alpha

---
+++ src/main/java/Convoy.java
@@ -0,0 +1,5 @@
+public class Convoy {
+    public static void main (String[] args) {
+       System.out.println("convoy!");
+    }
+}
---
+++ src/test/java/Convoytests.java
@@ -0,0 +1,5 @@
+public class Convoytests {
+    public static void main(String[] args) {
+        Convoy.main(null);
+    }
+}
